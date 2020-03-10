0fc8747ccf96d1cbae8b0caa8b2d574a1bc47f7e
David Adams <david.riding.rails@gmail.com>
2020-03-09 19:50:34 -0700

    add exercise 1

--- js_functions.js
+++ js_functions.js
@@ -1,6 +1,25 @@
 //Exercise 1
 //calculating gratuity
 
+let billAmount = 113.56;
+
+//Use function assignment syntax to avoid hoisting
+let gratuity = function(bill){
+  return bill * 0.2;
+};
+
+// let gratuity = (bill) => {
+//   return bill * 0.2;
+// };
+
+let withTotalGrat = function(amount){
+  let tip = gratuity(amount);
+  let total = amount + tip;
+  return total;
+};
+
+console.log("Your total including gratuity is: $" + withTotalGrat(billAmount).toFixed(2));
+
 
 
 
