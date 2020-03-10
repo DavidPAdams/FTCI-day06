d4bafeda7910289b9c8509adc1176c280c7fdd51
David Adams <david.riding.rails@gmail.com>
2020-03-09 22:36:01 -0700

    add jq_validations files

---
+++ jquery_validation/assets/javascripts/jq_validations.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  $("#submit").on('click', function() {
+
+    let name = $("#name");
+    let email = $("#email");
+    let message = $("#message");
+    let phone = $("#phone");
+
+    let required = [name, email, phone];
+
+    for (let i = 0; i < required.length; i++) {
+      if (required[i].val() === "") {
+        $(message).text("Please Fill Out Required Fields").addClass('warning');
+        required[i].prev().addClass('warning');
+      } else {
+        required[i].prev().removeClass('warning');
+      }
+    }
+
+    if (!$("#form label").hasClass('warning')) {
+      $("#form")[0].remove();
+      $("#pre-form h2").text("Thanks for your feedback!")
+    }
+
+  });
+});
